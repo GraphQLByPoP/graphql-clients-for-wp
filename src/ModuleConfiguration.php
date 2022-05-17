2d04d62e295f3f30e44c4775a57aa1d7a5b9172b
leoloso <leo@getpop.org>
2022-05-17 19:50:57 +0000

    Merge pull request #1589 from leoloso/Rename-Component-to-Module-and-viceversa
    Renamed Component to Module

--- src/ModuleConfiguration.php
+++ src/ModuleConfiguration.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace GraphQLByPoP\GraphQLClientsForWP;
 
-use PoP\Root\Component\AbstractComponentConfiguration;
+use PoP\Root\Module\AbstractModuleConfiguration;
 use PoPAPI\APIEndpoints\EndpointUtils;
-use PoP\Root\Component\EnvironmentValueHelpers;
+use PoP\Root\Module\EnvironmentValueHelpers;
 
-class ComponentConfiguration extends AbstractComponentConfiguration
+class ModuleConfiguration extends AbstractModuleConfiguration
 {
     /**
      * URL under which the clients are loaded.
